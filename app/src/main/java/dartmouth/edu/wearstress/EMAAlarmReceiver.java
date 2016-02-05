5ba942b834141682c840513a0ca505ca87c5b235
rwangfengme <_ReacTor@oumatoair.wireless.dartmouth.edu>
2016-02-05 18:59:55 -0500

    vibrate & boot
    vibrate & boot

---
+++ app/src/main/java/dartmouth/edu/wearstress/EMAAlarmReceiver.java
@@ -0,0 +1,25 @@
+package dartmouth.edu.wearstress;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by varun on 1/20/15.
+ */
+
+public class EMAAlarmReceiver extends BroadcastReceiver {
+    //Receive broadcas
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        startPSM(context);
+    }
+
+    // start the stress meter
+    private void startPSM(Context context) {
+        Intent emaIntent = new Intent(context, MainActivity.class); //The activity you  want to start.
+        emaIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(emaIntent);
+    }
+}
--- app/src/main/java/dartmouth/edu/wearstress/MainActivity.java
+++ app/src/main/java/dartmouth/edu/wearstress/MainActivity.java
@@ -1,7 +1,11 @@
 package dartmouth.edu.wearstress;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.GridViewPager;
 import android.util.Log;
@@ -15,6 +19,20 @@ public class MainActivity extends WearableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ComponentName receiver = new ComponentName(this, dartmouth.edu.wearstress.EMAAlarmReceiver.class);
+        PackageManager pm = this.getPackageManager();
+
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+        PSMScheduler psm = new PSMScheduler();
+        psm.setSchedule(this);
+
+        Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(500);
+
         setAdapter();
     }
 
